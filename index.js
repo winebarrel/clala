7a94da499eff588b05b4932c4fc4ad3202518819
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-12-21 21:53:38 +0900

    Fix

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 var Clala = {
   Client:   require('./lib/client'),
   evaluate: require('./lib/evaluate'),
-  Parser:   require('./lib/parser')
+  Parser:   require('./lib/parser'),
+  Lambda:   require('./lib/lambda')
 };
 
 module.exports = Clala;
